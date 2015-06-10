2ce2538a0fcada801156b37bd9d5e9c68778083e
cassioliveira <cassio@cassioliveira.com.br>
2015-06-10 07:28:03 -0300

    Removendo importação não usada

--- SISTEMA/002-IMPLEMENTACAO/InfoSaudeAC/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/atendimento/modelo/Enfermeiro.java
+++ SISTEMA/002-IMPLEMENTACAO/InfoSaudeAC/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/atendimento/modelo/Enfermeiro.java
@@ -1,7 +1,6 @@
 package br.edu.ifpb.monteiro.ads.infosaude.atendimento.modelo;
 
 import java.io.Serializable;
-import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
