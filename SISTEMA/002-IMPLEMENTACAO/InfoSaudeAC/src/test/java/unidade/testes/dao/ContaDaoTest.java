7e92c129078c66443ea0ab0c3da53750e3c1b6e9
WildeArruda <wildearruda@gmail.com>
2015-11-17 08:40:23 -0300

    Adicionada duas classes de testes em dao e em services, referentes a Conta. Teste em Dao com um teste comentado por indefinição necessária de reunião com o cliente sobre estratégia a ser usada.

---
+++ SISTEMA/002-IMPLEMENTACAO/InfoSaudeAC/src/test/java/unidade/testes/dao/ContaDaoTest.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidade.testes.dao;
+
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.controladores.DateTimeUtilBean;
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.dao.ContaDao;
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.enumeracoes.Permissao;
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.modelo.Conta;
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.util.jpa.EntityManagerProducer;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author wilde
+ */
+public class ContaDaoTest {
+    
+    private static EntityManagerProducer entityManagerProducer;
+    private static EntityManager entityManager;
+    private static ContaDao contaDao;
+    private static DateTimeUtilBean dateTimeUtilBean;
+    private static List<Conta> listaContas;
+    private static Long id;
+    
+    public ContaDaoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        contaDao = new ContaDao();
+        dateTimeUtilBean = new DateTimeUtilBean();
+        entityManagerProducer = new EntityManagerProducer("InfoSaudePU_Testes");
+        entityManager = entityManagerProducer.create();
+        contaDao.setEntityManager(entityManager);
+    }
+    
+    @Test(expected = Exception.class)
+    public void testSalvarContaNull() {
+
+        Conta conta = new Conta();
+
+        contaDao.getEntityManager().getTransaction().begin();
+        contaDao.salvar(conta);
+        contaDao.getEntityManager().getTransaction().commit();
+
+    }
+    
+    @Test
+    public void testSalvarContaCamposObrigatorios() {
+
+        Conta conta = new Conta();
+        
+        conta.setPassword("default");
+        conta.setPermissao(Permissao.ADMIN);
+        conta.setUserName("default");
+
+        contaDao.getEntityManager().getTransaction().begin();
+        contaDao.salvar(conta);
+        contaDao.getEntityManager().getTransaction().commit();
+
+        listaContas = (List<Conta>) new ArrayList<Conta>();
+        listaContas = contaDao.findAll();
+
+        for (Conta conta1 : listaContas) {
+
+            if (conta1.getUserName().equals(conta.getUserName())) {
+                id = conta1.getId();
+            }
+        }
+
+        Conta novaConta = new Conta();
+        novaConta = contaDao.findById(id);
+
+        assertEquals(conta.getPassword(), novaConta.getPassword());
+        assertEquals(conta.getPermissao(), novaConta.getPermissao());
+    }
+
+    //Teste comentado para decisão junto com o administrador para resolução de regra de negócio
+    
+//    @Test(expected = Exception.class)
+//    public void testSalvarContaDuplicada() {
+//
+//        Conta conta = new Conta();
+//        
+//        conta.setPassword("default1");
+//        conta.setPermissao(Permissao.ADMIN);
+//        conta.setUserName("default1");
+//
+//        contaDao.getEntityManager().getTransaction().begin();
+//        contaDao.salvar(conta);
+//        contaDao.getEntityManager().getTransaction().commit();
+//        
+//        contaDao.getEntityManager().getTransaction().begin();
+//        contaDao.salvar(conta);
+//        contaDao.getEntityManager().getTransaction().commit();
+//
+//    }
+}
---
+++ SISTEMA/002-IMPLEMENTACAO/InfoSaudeAC/src/test/java/unidade/testes/servicos/ContaServiceTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidade.testes.servicos;
+
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.enumeracoes.Permissao;
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.modelo.Conta;
+import br.edu.ifpb.monteiro.ads.infosaude.atendimento.servicos.ContaService;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+/**
+ *
+ * @author wilde
+ */
+public class ContaServiceTest {
+    
+    private static Conta conta;
+    private static Conta novaConta;
+
+    @Mock
+    private ContaService contaService;
+    
+    public ContaServiceTest() {
+        
+        contaService = new ContaService();
+        
+    }
+    
+    @Before
+    public void setUp() {
+        
+        MockitoAnnotations.initMocks(this);
+        conta = new Conta();
+        novaConta = new Conta();
+        
+    }
+    
+    @Test
+    public void testSaveCamposObrigatorios() {
+
+        conta.setPassword("123");
+        conta.setUserName("ubs");
+        conta.setPermissao(Permissao.MEDICO);
+
+        when(contaService.findById(1L)).thenReturn(conta);
+
+        novaConta = contaService.findById(1L);
+
+        assertEquals ("123", novaConta.getPassword());
+        assertEquals ("ubs", novaConta.getUserName());
+        assertEquals(conta.getPermissao(), novaConta.getPermissao());
+    }
+    
+    @Test(expected = Exception.class)
+    public void testFindByIdInexistente() {
+        when(contaService.findById(0L)).thenThrow(new Exception());
+        conta = contaService.findById(0L);
+    }
+    
+    @Test
+    public void testFindAll() {
+
+        List<Conta> listaContas = new ArrayList<>();
+
+        conta.setUserName("cool");
+        conta.setPassword("cool");
+        conta.setPermissao(Permissao.ADMIN);
+        
+        listaContas.add(conta);
+
+        when(contaService.findAll()).thenReturn(listaContas);
+
+        assertFalse(listaContas.isEmpty());
+
+    }
+}
